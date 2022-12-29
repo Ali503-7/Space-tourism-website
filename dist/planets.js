e6cef43fceea7fdbbb08f21c56af9412d9f9497e
Ali503-7 <alimoali354@gmail.com>
2022-12-29 19:44:39 +0200

    done 100% of the planets page yayyyyyyyyy

--- dist/planets.js
+++ dist/planets.js
@@ -4,14 +4,21 @@ let P_Name = document.querySelector(".mid h2");
 let P_doc = document.querySelector('.mid p');
 let Km = document.querySelector('.KM');
 let Days = document.querySelector('.Days');
-let P_ul = document.querySelectorAll('.top ul li');
-P_ul.forEach(_li => {
-    _li.onclick = () => {
-        planets();
+let P_ul;
+P_ul = document.querySelectorAll('.top ul li');
+let reset = () => {
+    P_ul.forEach((_li) => {
+        _li.setAttribute("status", '');
+    });
+};
+P_ul.forEach((li) => {
+    li.onclick = () => {
+        planets(li.getAttribute('name'));
+        reset();
+        li.setAttribute('status', 'active');
     };
 });
-planets();
-function planets() {
+function planets(_theplanet) {
     fetch(`../data.json`).then((data) => {
         return data.json();
     })
@@ -19,6 +26,16 @@ function planets() {
         return planets.destinations;
     })
         .then((_opjects) => {
+        _opjects.forEach((namee) => {
+            if (namee.name == _theplanet) {
+                P_Name.innerHTML = namee.name;
+                P_doc.innerHTML = namee.description;
+                P_img.src = namee.images.png;
+                Km.innerHTML = namee.distance;
+                Days.innerHTML = namee.travel;
+            }
+        });
     });
 }
+planets('Moon');
 //# sourceMappingURL=planets.js.map
--- src/planets.ts
+++ src/planets.ts
@@ -1,19 +1,32 @@
-let P_img = document.querySelector('.P_img')
-let P_Name = document.querySelector(".mid h2")
-let P_doc = document.querySelector('.mid p')
-let Km = document.querySelector('.KM')
-let Days = document.querySelector('.Days')
+let P_img: any = document.querySelector('.P_img')
+let P_Name: any = document.querySelector(".mid h2")
+let P_doc: any = document.querySelector('.mid p')
+let Km: any = document.querySelector('.KM')
+let Days: any = document.querySelector('.Days')
 
-let P_ul = document.querySelectorAll('.top ul li')
+let P_ul: any;
 
-P_ul.forEach(_li => {
-  _li.onclick = () => {
-    planets()
+P_ul = document.querySelectorAll('.top ul li');
+
+let reset = () =>{
+  P_ul.forEach((_li: any) => {
+    _li.setAttribute("status", '')
+  });
+}
+
+
+P_ul.forEach((li: {
+  [x: string]: any; onclick: () => void; getAttribute: (arg0: string) => string 
+}) => {
+  li.onclick = () => {
+    planets(li.getAttribute('name'))
+    reset()
+    li.setAttribute('status', 'active')
   }
 });
 
-planets()
-function planets() {
+
+function planets(_theplanet: string): void {
   fetch(`../data.json`).then((data) => {
     return data.json()
   })
@@ -21,7 +34,18 @@ function planets() {
       return planets.destinations
     })
     .then((_opjects) => {
-
+      _opjects.forEach((namee: {
+        [x: string]: any
+        description: string ;name: string 
+}) => {
+        if (namee.name == _theplanet) {
+          P_Name.innerHTML = namee.name
+          P_doc.innerHTML = namee.description
+          P_img.src = namee.images.png
+          Km.innerHTML = namee.distance
+          Days.innerHTML = namee.travel
+        }
+      });
     })
 }
-
+planets('Moon')
