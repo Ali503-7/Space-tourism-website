c2715ff3f45cb625ef523f8df1ca6ff1911adfa5
Ali503-7 <alimoali354@gmail.com>
2022-12-30 21:27:06 +0200

    done all the crew page

--- dist/header.js
+++ dist/header.js
@@ -9,8 +9,8 @@ let header = '<div class="head contaner">' +
     '<ul>' +
     '<li><a href="./index.html" class="BarC " status="" name="home"><span class="numbering">00</span>Home</a></li>' +
     '<li><a href="./destination-moon.html" class="BarC" status="" name="destination"><span class="numbering">01</span>Destination</a></li>' +
-    '<li><a href="../crew-commander.html" class="BarC" status="" name="crew"><span class="numbering">02</span>Crew</a></li>' +
-    '<li><a href="../technology-vehicle.html" class="BarC" status="" name="technology"><span class="numbering">03</span>Technology</a>' +
+    '<li><a href="./crew-commander.html" class="BarC" status="" name="crew"><span class="numbering">02</span>Crew</a></li>' +
+    '<li><a href="./technology-vehicle.html" class="BarC" status="" name="technology"><span class="numbering">03</span>Technology</a>' +
     '</li>' +
     '</ul>' +
     '</div>' +
@@ -19,8 +19,8 @@ let header = '<div class="head contaner">' +
     '<ul>' +
     '<li><a href="./index.html" class="BarC" status="" name="home"><span class="numbering">00</span>Home</a></li>' +
     '<li><a href="./destination-moon.html" class="BarC" status="" name="destination"><span class="numbering">01</span>Destination</a></li>' +
-    '<li><a href="../crew-commander.html" class="BarC" status="" name="crew"><span class="numbering">02</span>Crew</a></li>' +
-    '<li><a href="../technology-vehicle.html" class="BarC" status="" name="technology"><span class="numbering">03</span>Technology</a>' +
+    '<li><a href="./crew-commander.html" class="BarC" status="" name="crew"><span class="numbering">02</span>Crew</a></li>' +
+    '<li><a href="./technology-vehicle.html" class="BarC" status="" name="technology"><span class="numbering">03</span>Technology</a>' +
     '</li>' +
     '</ul>' +
     '</div>' +
